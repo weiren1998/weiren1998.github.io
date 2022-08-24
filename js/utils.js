71a5f5a9196a1f9d006de15e2ddd02659b9b0718
weiren1998 <weiren.pku@gmail.com>
2022-08-25 00:43:48 +0800

    Site updated: 2022-08-25 00:43:46

--- js/utils.js
+++ js/utils.js
@@ -220,7 +220,7 @@ NexT.utils = {
       // TOC item animation navigate.
       link.addEventListener('click', event => {
         event.preventDefault();
-        var target = document.getElementById(event.currentTarget.getAttribute('href').replace('#', ''));
+        var target = document.getElementById(decodeURI(event.currentTarget.getAttribute('href').replace('#', '')));
         var offset = target.getBoundingClientRect().top + window.scrollY;
         window.anime({
           targets  : document.scrollingElement,
@@ -229,7 +229,7 @@ NexT.utils = {
           scrollTop: offset + 10
         });
       });
-      return document.getElementById(link.getAttribute('href').replace('#', ''));
+      return document.getElementById(decodeURI(link.getAttribute('href').replace('#', '')));
     });
 
     var tocElement = document.querySelector('.post-toc-wrap');
